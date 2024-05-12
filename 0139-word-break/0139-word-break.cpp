5fb238d8537c47b4474bcccc02e234d7ee4c84af
Sainesh Nakra <30744887+saineshnakra@users.noreply.github.com>
2024-05-12 16:25:05 -0400

    Time: 20 ms (23.06%), Space: 19.1 MB (15.55%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool>dp(s.size()+1,false);
         unordered_map<string, bool>m;
-        if(s.size()==0){
-            return true;
-        }
+        dp[0] = true;
         for(int i = 0;i<wordDict.size();i++){
             m[wordDict[i]] = true;
         }
-        int start = 0;
-        int finish = s.size();
-        while(start<s.size()){
-            cout<<s.substr(start,finish)<<endl;
-            if(m[s.substr(start,finish)]){
-                s = s.substr(finish,s.size());
-                finish = s.size();
-                if(s.size()==0){
-                    cout<<"Exit here and true";
-                    return true;
+        for(int i = 1;i<=s.size();i++){
+            for(int j = 0;j<i;j++){
+                if(dp[j]&&m[s.substr(j,i-j)]){
+                    dp[i] = true;
                 }
-            }
-            else{
-                if(start>=finish){
-                    cout<<start<<" "<<finish<<endl;
-                    cout<<"Exit here and false";
-                    return false;
-                }
-                finish--;
             }
         }
-        return false;
+        return dp[s.size()];
     }
 };
