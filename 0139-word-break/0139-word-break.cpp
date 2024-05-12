7bc45124ba42309e3d2ce93a764f0865bb876e37
Sainesh Nakra <30744887+saineshnakra@users.noreply.github.com>
2024-05-12 16:25:25 -0400

    Time: 20 ms (23.06%), Space: 18.9 MB (16.53%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -11,6 +11,7 @@ public:
             for(int j = 0;j<i;j++){
                 if(dp[j]&&m[s.substr(j,i-j)]){
                     dp[i] = true;
+                    continue;
                 }
             }
         }
