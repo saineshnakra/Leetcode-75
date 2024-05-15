01e4c6f504d82a2ccae00993d71033a1965efeb3
Sainesh Nakra <30744887+saineshnakra@users.noreply.github.com>
2024-05-15 06:29:29 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        bool flag = false;
-        bool digitStarted = false;
-        bool sign = true;
-        int res = 0;
-        for(int i = 0;i<s.size();i++){
-            if(flag==false&&s.at(i)==' '){
-                continue;
-            }
-            else if(digitStarted&&(s.at(i)-'0'>9||s.at(i)-'0'<0)){
-                return res;
-            }
-            else if(s.at(i)=='+'&&flag==false){
-                sign = true;
-                flag = true;
-            }
-            else if(s.at(i)=='+'&&flag==true){
-                return 0;
-            }
-            else if(s.at(i)=='-'&&flag==true){
-                return 0;
-            }
-            else if(s.at(i)=='-'&&flag==false){
-                sign = false;
-                flag = true;
-
-            }
-            else if(s.at(i)==' '&&flag!=false){
-                break;
-            }
-            else if(s.at(i)-'0'>=0&s.at(i)-'0'<=9){
-            int digit = s[i] - '0';
-            if (sign && res > (INT_MAX - digit) / 10) return INT_MAX;
-            if (!sign && res < (INT_MIN + digit) / 10) return INT_MIN;
-                if(sign){
-                res = res*10 + digit;
+        string trim_s = s;
+        string res = "";
+        char sign = 'n';
+        int k = 0;
+            while(k<trim_s.size()&&trim_s[k]-'0'<0||trim_s[k]-'0'>9){
+                if(trim_s[k]!=' '&&trim_s[k]!='+'&&trim_s[k]!='-'){
+                    return 0;
                 }
                 else{
-                    res = res*10-digit;
+                    if(sign!='n'){
+                            return 0;
+                        }
+                    if(trim_s[k]=='+'||trim_s[k]=='-'){
+                        sign = trim_s[k];
+                    }
                 }
-                flag = true;
-                digitStarted = true;
+                k++;
             }
-            else{
-                break;
+            while(k<trim_s.size()&&trim_s[k]-'0'>=0&&trim_s[k]-'0'<=9){
+                res += trim_s[k];
+                k++;
             }
-        }
-        return res;
+            unsigned long ans = 0;
+            for(int i = 0;i<res.size();i++){
+                ans = ans*10 + (res[i]-'0');
+            }
+            if (ans >= pow(2,31) and sign=='-'){   
+                ans = pow(2,31);
+            } 
+            else if (ans >= pow(2,31)){
+            ans = pow(2,31)-1;
+            } 
+
+            return sign=='+'||sign=='n'?ans:-ans;
+           
     }
 };
