957731e16c7312578654ba437ee510ee7497a4a1
Sainesh Nakra <30744887+saineshnakra@users.noreply.github.com>
2024-07-08 16:30:26 -0400

    Time: 0 ms (100%), Space: 7 MB (98.8%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-int josephus(int n, int k) {
-        if (n == 1) {
+    int joseph(int n, int k){
+        if(n==1){
             return 0;
         }
-        return ((josephus(n - 1, k) + k) % n);
+        return (joseph(n-1, k) + k)%n;
     }
-
     int findTheWinner(int n, int k) {
-        return josephus(n, k) + 1;
+        return joseph(n,k)+1;
     }
 };
