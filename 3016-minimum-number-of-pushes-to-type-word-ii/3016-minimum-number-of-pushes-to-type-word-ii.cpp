ccccfe268da5f8c125065562e9970725aa3f1587
Sainesh Nakra <30744887+saineshnakra@users.noreply.github.com>
2024-08-06 19:20:48 -0400

    Time: 127 ms (20.63%), Space: 29.3 MB (5.3%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -4,24 +4,20 @@ public:
         int res = 0;
         unordered_map<char, int> m;
         
-        // Count frequency of each character
         for (char c : word) {
             m[c]++;
         }
         
-        // Collect frequencies into a vector
         vector<int> frequencies;
         for (const auto& entry : m) {
             frequencies.push_back(entry.second);
         }
         
-        // Sort frequencies in descending order
         sort(frequencies.rbegin(), frequencies.rend());
         
         int count = 1;
         int assigned = 0;
         
-        // Calculate minimum pushes
         for (int freq : frequencies) {
             res += count * freq;
             assigned++;
