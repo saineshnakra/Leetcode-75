16a03edd369c6a1e58a7d6f7e8103b64565eb7ea
Sainesh Nakra <30744887+saineshnakra@users.noreply.github.com>
2024-09-05 10:47:39 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        mini = 1001
+        res = 0
+        for curr in prices:
+            if mini>curr:
+                mini = curr
+            else:
+                res = max(curr - mini,res)
+        return res
+        
